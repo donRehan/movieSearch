aeef3249ec1ed1c11e2ccbbf144671277b213492
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-10-08 08:35:48 +0300

    Finish movie list page

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { useMovieStore } from '../stores/useMovieStore';
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { Star } from 'lucide-react';
+import style from './page.module.css';
 
 const Page: React.FC<any>= (search) => {
   const {selectedMovie, setSelectedMovie} = useMovieStore();
@@ -26,33 +28,58 @@ const Page: React.FC<any>= (search) => {
     fetchMovies();
   }, [search]);
 
+  const handleFavorite = (e: React.MouseEvent, movieId: number) => {
+    e.preventDefault()
+    console.log('Toggling favorite for movie:', movieId)
+  }
+
 
   const handleMovieClick = (movie: any) => {
     setSelectedMovie(movie);
   };
 
-  let width = 300;
+  let width = 100;
+  let height = 150;
 
+  //TODO: Display a message if no movies are found.
+  //TODO: Display a message if input was given from the user
+  //TODO: Display a message if end point had an issue
   return (
   <>
-  <div>
-    {movies.map((movie: any) => (
-      <div onClick={()=> handleMovieClick(movie)}>
-        <Link href={
-          {
-            pathname: `/title`,
-            query: { movie: JSON.stringify(movie) }
-          }
-        }>
-          <Image width={width} height={width} src={`https://image.tmdb.org/t/p/w${width}/${movie.poster_path}`} alt={movie.title} />
-          <h2>{movie.title}</h2>
-          <p>{movie.release_date}</p>
-          <p>{movie.vote_average}</p>
-        </Link>
-        <button>Add to favorites</button>
-      </div>
-    ))}
-  </div>
+    <div className={style.movieList}>
+      <h1 className={style.h1}>Movie List</h1>
+      <ul className={style.ul}>
+        {movies.map((movie: any) => (
+            <Link href={
+              {
+                pathname: `/title`,
+                query: { movie: JSON.stringify(movie) }
+              }
+            }
+            passHref
+            legacyBehavior
+            >
+            <a className={style.movieLink}>
+          <li key={movie.id} className={style.movieItem}>
+            <Image className={style.moviePoster} width={width} height={height} src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={`${movie.title} poster`} />
+            <div className={style.movieInfo}>
+              <h2 className={style.h2}>{movie.title}</h2>
+              <p className={style.p}>Release Date: {movie.release_date}</p>
+              <p className={style.p}>Rating: {movie.vote_average.toFixed(1)}/10</p>
+            </div>
+            <button 
+            className={`${style.favoriteButton} ${movie.isFavorite ? 'is-favorite' : ''}`}
+            onClick={(e) => handleFavorite(e, movie.id)}
+            >
+              <Star />
+            </button>
+          </li>
+            </a>
+            </Link>
+        ))}
+      </ul>
+    </div>
+
   </>
   );
 }
