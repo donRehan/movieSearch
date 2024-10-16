aacf48c665193a471bd5ebd22a9e5b3440446c2b
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-10-17 02:10:33 +0300

    Fix BUG
    Previous commit introduced a bug where pressing enter wouldn't fire a
    search query.
    BUG was calling the function like this function_name
    instead of function_name()

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 import Button from './ui/Button/Button'
 
@@ -15,11 +15,25 @@ export default function Page() {
       setError(true);
   }
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.code === 'Enter')
+      handleSearch();
+  }
+
   return (
   <main className={styles.main}>
     <div className={styles.searchbar}>
       <div className={styles.title}>Search Movie</div>
-        <input className={styles.searchformInput} type="text" value={search} placeholder="Enter movie title..." onChange={e => setSearch(e.target.value)} name="search" autoComplete="off"/>
+        <input
+        className={styles.searchformInput}
+        type="text"
+        value={search}
+        placeholder="Enter movie title..."
+        onChange={e => setSearch(e.target.value)}
+        name="search"
+        autoComplete="off"
+        onKeyPress={handleKeyPress}
+        />
         <Button onclick={handleSearch}>
           <svg className={styles.searchIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -28,7 +42,9 @@ export default function Page() {
         </Button>
         {
           error && 
-          <p className={styles.errorMessage} role="alert">Please enter a movie title before clicking search. </p>
+          <p className={styles.errorMessage} role="alert">
+          Please enter a movie title before clicking search. 
+          </p>
         }
     </div>
   </main>
