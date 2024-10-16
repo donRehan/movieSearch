8c67429313fd8207c2be6bf7f78e81c84063aac7
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-10-16 22:17:08 +0300

    Add changes discussed in noon meeting in main page && Button element
    . Add onclick to button props , type.
    . Make submit without using form.
    . Accept keyboard stroke using an event listener.
    . Prevent user from entering no search-term && display proper error
    message.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,35 @@
+"use client";
+
+import { useState } from 'react';
 import styles from './page.module.css';
 import Button from './ui/Button/Button'
 
 export default function Page() {
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSearch = () => {
+    if(search)
+      window.location.assign(`list?search=${search}`);
+    else
+      setError(true);
+  }
 
   return (
   <main className={styles.main}>
     <div className={styles.searchbar}>
       <div className={styles.title}>Search Movie</div>
-      <form className={styles.searchForm} action="/list" method="GET"> 
-        <input className={styles.searchformInput} type="text" placeholder="Enter movie title..." name="search" autoComplete="off"/>
-        <Button type="submit">
+        <input className={styles.searchformInput} type="text" value={search} placeholder="Enter movie title..." onChange={e => setSearch(e.target.value)} name="search" autoComplete="off"/>
+        <Button onclick={handleSearch}>
           <svg className={styles.searchIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
           Search
         </Button>
-      </form>
+        {
+          error && 
+          <p className={styles.errorMessage} role="alert">Please enter a movie title before clicking search. </p>
+        }
     </div>
   </main>
   );
--- app/ui/Button/Button.tsx
+++ app/ui/Button/Button.tsx
@@ -5,9 +5,10 @@ import styles from './Button.module.css';
 interface ButtonProps {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
+  onclick: () => void;
 }
 
-export default function Button({ children , type}: ButtonProps) {
+export default function Button({ children, type, onclick }: ButtonProps) {
   let has_type: boolean;
   has_type = (type !== undefined);
 
@@ -15,10 +16,11 @@ export default function Button({ children , type}: ButtonProps) {
     <>
       {
         has_type ?
-        <button className={styles.button} type={type}>
+        <button className={styles.button} onClick={onclick} type={type}>
           {children}
-        </button>:
-        <button className={styles.button}>
+        </button>
+        :
+        <button className={styles.button} onClick={onclick}>
           {children}
         </button>
       }
